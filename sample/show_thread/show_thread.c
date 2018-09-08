8b1e31900463ba6cbbf9af3a3a80234c5d0eb4dd
akh <akhs3214@gmail.com>
2018-09-08 09:34:36 +0900

    [WIP] Fix names

--- sample/show_thread/show_thread.c
+++ sample/show_thread/show_thread.c
@@ -7,15 +7,15 @@
 #define MAX_THREAD 8
 
 static void *subthread(void *vMsgQue) {
-    MSG_QUEUE_T *msgQue = (MSG_QUEUE_T *)vMsgQue;
+    msgq_t *msgQue = (msgq_t *)vMsgQue;
     ULONG msg[4] = {0};
-    MSG_QUE_READ(msgQue, msg, sizeof(msg), 0);
+    msgq_receive(msgQue, msg, sizeof(msg), 0);
     printf("Thread: %lu\n", msg[0]);
     return NULL;
 }
 
 int main(void) {
-    MSG_QUEUE_T *msgQueue[MAX_THREAD];
+    msgq_t *msgQueue[MAX_THREAD];
     ULONG msg[4] = {0};
     char *tmp[MAX_THREAD];
     pthread_t tid[MAX_THREAD];
@@ -25,12 +25,12 @@ int main(void) {
     }
 
     for (int i = 0; i < MAX_THREAD; i++) {
-        msgQueue[i] = MSG_QUE_CREATE(tmp[i]);
+        msgQueue[i] = msgq_open(tmp[i], 0);
         pthread_create(&tid[i], NULL, subthread, msgQueue[i]);
     }
     for (int i = 0; i < MAX_THREAD; i++) {
         msg[0] = i;
-        if (MSG_QUE_SEND(msgQueue[i], msg, sizeof(msg), 0) == -1) {
+        if (msgq_send(msgQueue[i], msg, sizeof(msg), 0) == -1) {
             PERROR(__FUNCTION__);
             exit(EXIT_FAILURE);
         }
@@ -39,7 +39,7 @@ int main(void) {
         pthread_join(tid[i], NULL);
     }
     for (int i = 0; i < MAX_THREAD; i++) {
-        MSG_QUE_CLOSE(msgQueue[i]);
+        msgq_close(msgQueue[i]);
     }
     return EXIT_SUCCESS;
 }
